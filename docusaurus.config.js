28b6a5c0a7c9cd651eea8405d73b819baafbdb97
Dakota Chambers <dakotachambers@gmail.com>
2023-11-02 21:35:29 -0500

    fix(docs): Change edit url to correct github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/digitalducky/handbook',
+            'https://github.com/digitalducky/handbook/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/digitalducky/handbook',
+            'https://github.com/digitalducky/handbook/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
